a13edebbd639ccebc4d1611412af3d2cbb479525
snehaM2210 <snehamallik665@gmail.com>
2021-04-25 14:59:15 +0530

    app initialised

---
+++ weather-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Weather from './app_component/weather';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+    </div>
+  );
+}
+
+export default App;
