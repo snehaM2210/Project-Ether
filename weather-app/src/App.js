17d829b4d82d239217aab980cce7fee40daa2938
deb2000-sudo <59384249+deb2000-sudo@users.noreply.github.com>
2021-05-05 00:36:34 +0530

    pushed file

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,12 +4,66 @@ import Weather from './app_component/weather';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Weather/>
-    </div>
-  );
-}
+// npm i bootstrap
+// npm i https://github.com/erikflowers/weather-icons.git
 
+//api call api.openweathermap.org/data/2.5/weather?q=London&appid=
+const Api_Key = "727fc0701eaff113b26258dcf5e7a208";
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celsius: undefined,
+      temp_max: undefined,
+      temp_min: undefined,
+      description:"",
+      error:false
+
+
+    };
+    this.getWeather();
+  }
+
+  calCelsius(temp){
+    let cell = Math.floor(temp-273.15);
+    return cell;
+  }
+  
+  getWeather = async () => {
+    const api_call = await fetch(
+  
+    `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${Api_Key}`
+    );
+
+    const response = await api_call.json();
+    console.log(response);
+    this.setState({
+      city:response.name,
+      country:response.sys.country,
+      celsius:this.calCelsius
+
+    });
+  }
+   render(){
+    return (
+      <div className="App">
+        <Weather city={this.state.city}
+          country={this.state.country}
+          temp_celsius={this.state.celsius}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
+          description={this.state.description}
+          />
+      </div>
+    );
+      
+   }
+ 
+}
 export default App;
+
--- weather-app/src/app_component/weather.jsx
+++ weather-app/src/app_component/weather.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Weather = () => {
+const Weather = props => {
     return(
         <div className="container">
             <div className="cards">
-                <h1>London</h1>
+                <h1>
+                    {props.city},{props.country}
+                </h1>
                     <h5 className="py-4">
                         <i className="wi wi-day-sunny display-1"></i>
                     </h5>
