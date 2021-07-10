2f0bf75936f6eb516d207a7f626645f6cce610dd
snehaM2210 <snehamallik665@gmail.com>
2021-07-10 22:14:01 +0530

    Update App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import Form from "./app_component/form.component";
 import Weather from './app_component/weather';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'weather-icons/css/weather-icons.css';
+import "weather-icons/css/weather-icons.css";
 
 // npm i bootstrap
 // npm i https://github.com/erikflowers/weather-icons.git
@@ -11,7 +12,7 @@ import 'weather-icons/css/weather-icons.css';
 const Api_Key = "727fc0701eaff113b26258dcf5e7a208";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state ={
       city: undefined,
@@ -19,14 +20,49 @@ class App extends React.Component {
       icon: undefined,
       main: undefined,
       celsius: undefined,
-      temp_max: undefined,
-      temp_min: undefined,
-      description:"",
+      temp_max: null,
+      temp_min: null,
+      description: "",
       error:false
-
-
     };
-    this.getWeather();
+
+    this.weatherIcon={
+      Thunderstorm:"wi-thunderstorm",
+      Drizzle:"wi-sleet",
+      Rain:"wi-storm-showers",
+      Snow:"wi-snow",
+      Atmosphere:"wi-fog",
+      Clear:"wi-day-sunny",
+      Clouds:"wi-day-fog"
+    };
+  }
+
+  get_WeatherIcon(icons,rangeId){
+    switch(true){
+       case rangeId >= 200 && rangeId < 232:
+        this.setState({ icon: icons.Thunderstorm });
+        break;
+      case rangeId >= 300 && rangeId <= 321:
+        this.setState({ icon: icons.Drizzle });
+        break;
+      case rangeId >= 500 && rangeId <= 521:
+        this.setState({ icon: icons.Rain });
+        break;
+      case rangeId >= 600 && rangeId <= 622:
+        this.setState({ icon: icons.Snow });
+        break;
+      case rangeId >= 701 && rangeId <= 781:
+        this.setState({ icon: icons.Atmosphere });
+        break;
+      case rangeId === 800:
+        this.setState({ icon: icons.Clear });
+        break;
+      case rangeId >= 801 && rangeId <= 804:
+        this.setState({ icon: icons.Clouds });
+        break;
+      default:
+        this.setState({ icon: icons.Clouds });
+    }
   }
 
   calCelsius(temp){
@@ -34,31 +70,54 @@ class App extends React.Component {
     return cell;
   }
   
-  getWeather = async () => {
+  getWeather = async e => {
+    e.preventDefault();
+
+    const country = e.target.elements.country.value;
+    const city = e.target.elements.city.value;
+
+    if (country && city) {
     const api_call = await fetch(
   
-    `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${Api_Key}`
+    `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
     );
 
     const response = await api_call.json();
-    console.log(response);
+    //console.log(response);
     this.setState({
-      city:response.name,
-      country:response.sys.country,
-      celsius:this.calCelsius
+        city: `${response.name}, ${response.sys.country}`,
+        country: response.sys.country,
+        main: response.weather[0].main,
+        celsius: this.calCelsius(response.main.temp),
+        temp_max: this.calCelsius(response.main.temp_max),
+        temp_min: this.calCelsius(response.main.temp_min),
+        description: response.weather[0].description,
+        error: false
+      });
+    // seting icons
+      this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
 
-    });
-  }
+      console.log(response);
+    } else {
+      this.setState({
+        error: true
+      });
+    }
+  };
+
    render(){
     return (
       <div className="App">
-        <Weather city={this.state.city}
+        <Form loadweather={this.getWeather} error={this.state.error} />
+        <Weather 
+          cityname={this.state.city}
           country={this.state.country}
           temp_celsius={this.state.celsius}
           temp_max={this.state.temp_max}
           temp_min={this.state.temp_min}
           description={this.state.description}
-          />
+          weatherIcon={this.state.icon}
+        />
       </div>
     );
       
