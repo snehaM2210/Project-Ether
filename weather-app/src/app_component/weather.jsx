41fd4d1f2e6bb0e6dcb737d5bdcc757f91a5a97b
snehaM2210 <snehamallik665@gmail.com>
2021-07-10 22:14:12 +0530

    Update weather.jsx

--- weather-app/src/app_component/weather.jsx
+++ weather-app/src/app_component/weather.jsx
@@ -1,38 +1,44 @@
 import React from 'react';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./weather.style.css";
 
 const Weather = props => {
-    return(
-        <div className="container">
-            <div className="cards">
-                <h1>
-                    {props.city},{props.country}
-                </h1>
-                    <h5 className="py-4">
-                        <i className="wi wi-day-sunny display-1"></i>
-                    </h5>
-                     <h1 className="py-2">25&deg;</h1>
+  return (
+    <div className="container text-light">
+      <div className="Card">
+        <h1 className="text-white py-3">{props.cityname}</h1>
+        <h5 className="py-4">
+          <i className={`wi ${props.weatherIcon} display-1`} />
+        </h5>
 
-                     {/** Max and Min temperature */}
-                     {minmaxTemp(24,19)}
+        {/* Get Celsius */}
+        {props.temp_celsius ? (
+          <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+        ) : null}
 
-                     <h4 className="py-3">
-                         Slow Rain
-                     </h4>
-            </div>
-        </div>
-    );
+        {/* show max and min temp */}
+        {maxminTemp(props.temp_min, props.temp_max)}
+
+        {/* Weather description */}
+        <h4 className="py-3">
+          {props.description.charAt(0).toUpperCase() +
+            props.description.slice(1)}
+        </h4>
+      </div>
+    </div>
+  );
 };
 
-
-function minmaxTemp(min,max){
-    return(
-        <h3>
-            <span className="px-4">{min}&deg;</span>
-            <span className="px-4">{max}&deg;</span>
-        </h3>
-    );
-}
-
 export default Weather;
+
+function maxminTemp(min, max) {
+  if (max && min) {
+    return (
+      <h3>
+        <span className="px-4">{min}&deg;</span>
+        <span className="px-4">{max}&deg;</span>
+      </h3>
+    );
+  }
+}
